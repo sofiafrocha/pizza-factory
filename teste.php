8050d5ceadb095617c4c0af1e9de21987b016a3f
Sofia F. Rocha <sfrocha@student.dei.uc.pt>
2014-12-05 02:40:39 +0000

    sql commands to add a basic pizza

--- teste.php
+++ teste.php
@@ -1,8 +1,9 @@
 <?php
+	session_start();
 	include "connect.php";
 
 	//fazer uma encomenda
-	$sql_enc="INSERT INTO ENCOMENDA (id_enc, id_cliente, data, estado) VALUES ('xxx', $_SESSION['id_cliente'], 'xxxx', 0)";
+	$sql_enc="INSERT INTO ENCOMENDA (id_cliente, estado) VALUES ($_SESSION['id_cliente'], 0)";
 
 	if (!mysqli_query($con,$sql_enc))
     {
@@ -11,8 +12,13 @@
 
 	//ir buscar o id da encomenda que acabou de fazer -_-
 	//listar todos, ordenar por data e limitar ao primeiro
+	$sql_getIdEnc="SELECT id_enc FROM ENCOMENDA ORDER BY data desc LIMIT 1";
+	$row = mysqli_fetch_array($sql_getIdEnc, MYSQLI_NUM);
+	
+	$id_enc = $row[0];
+
 	//fazer uma pizza
-	$sql_pizza="INSERT INTO PIZZA (id_pizza, id_enc, estado, tamanho, massa, tomate, queijo, data) VALUES ('xxx', 'xxxx', '0', 'alta', 'yes', 'yes', 'xxxxx')";
+	$sql_pizza="INSERT INTO PIZZA (id_enc, estado, tamanho, massa, tomate, queijo) VALUES ('$id_enc', '0', 'alta', 'yes', 'yes')";
 
 	if (!mysqli_query($con,$sql_pizza))
     {
@@ -21,8 +27,13 @@
 	
 	//encontrar id da pizza que acabámos de fazer -_-
 	//listar todos, ordenar por data e limitar ao primeiro
+	$sql_getIdPizza="SELECT id_pizza FROM PIZZA ORDER BY data desc LIMIT 1";
+	$row = mysqli_fetch_array($sql_getIdPizza, MYSQLI_NUM);
+	
+	$id_pizza= $row[0];
+
 	//por ingredientes na pizza
-	$sql_ingredientes="INSERT INTRO PIZZA_has_INGREDIENTE (id_ingredientes, id_pizza) VALUES ('xxx', 'xxx')";
+	$sql_ingredientes="INSERT INTRO PIZZA_has_INGREDIENTE (id_ingredientes, id_pizza) VALUES ('1', '$id_pizza'), ('2', '$id_pizza'), ('3', '$id_pizza'), ('4', '$id_pizza')";
 
 	if (!mysqli_query($con,$sql_ingredientes))
     {
