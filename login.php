d287a3c178c1bd7ce9c0f831ed4b88873b4841f9
Sofia F. Rocha <sfrocha@student.dei.uc.pt>
2014-12-10 00:23:00 +0000

    php and html to log in is on the same file

--- login.php
+++ login.php
@@ -1,6 +1,55 @@
 <?php
    session_start();
-   include "connect.php";
+
+   if(isset($_POST['action'])) 
+   {
+      include "php/connect.php";
+
+      echo "ele está a passar o username " . $_POST[username] . "<br>";
+
+      $sql_password = mysqli_query($con, "SELECT PASSWORD FROM `CLIENTE` where USERNAME = '$_POST[username]'");
+
+
+      if (!$sql_password)
+      {
+      die('Error: ' . mysqli_error($con));
+      }
+
+      $row = mysqli_fetch_assoc($sql_password);
+      $password = $row['PASSWORD'];
+
+      echo "sql return password " . $row['PASSWORD'] . "<br>";
+      echo "var password" . $password . "<br>";
+
+      //password está certa
+      if ($_POST[password] == $password){
+
+      $sql_user_id = mysqli_query($con, "SELECT ID_CLIENTE, NOME, USERNAME FROM `CLIENTE` where USERNAME = '$_POST[username]'");
+      $row = mysqli_fetch_assoc($sql_user_id);
+
+      //armazena user id
+      $_SESSION['user_id'] = $row['ID_CLIENTE'];
+      $_SESSION['name'] = $row['NOME'];
+      $_SESSION['username'] = $row['USERNAME'];
+      $_SESSION['logged_in'] = true;
+
+      echo "session user " . $_SESSION['user_id'] . "<br>";
+      echo "sql user " . $row['ID_CLIENTE'] . "<br>";
+
+      echo "login successfull"; 
+      //echo "<a href="../index.php">index</a>"; 
+      //por redirect 
+      }
+
+      //password está errada
+      if ($_POST[password] != $password){
+      echo "wrong password or username";
+      }
+
+      mysqli_close($db);
+
+      mysqli_close($con);
+   }
 ?>
 
 <html>
@@ -40,7 +89,7 @@
 
             <div class="col s6 m6 l6">
 
-               <form class="col offset-s3 s6 offset-s3" action="php/login.php" method="POST">
+               <form class="col offset-s3 s6 offset-s3" method="POST">
                   <input name="username" type="text" required autofocus>
                   <label for="username">username</label>
                   
