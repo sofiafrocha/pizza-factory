9afcf2f2a111ef4ba7b18bc69be1f8c654cf3cf0
Sofia F. Rocha <sfrocha@student.dei.uc.pt>
2014-12-11 18:23:21 +0000

    btns de mudar o estado já funcionam nas encomendas

--- listaEncomendas.php
+++ listaEncomendas.php
@@ -1,6 +1,38 @@
 <?php
 	session_start();
-	include "connect.php";
+	include "php/connect.php";
+
+	if(isset($_POST[state_changer])){
+		echo "who clicked the buttons?!" . "<br>";
+
+		echo "the lucky number is: " . $_POST[state_changer] .  "<br>";
+
+		//mudar o estado da pizza
+		$enc_id = $_POST[state_changer];
+		//encontrar estado actual
+		$sql_get_state = mysqli_query($con, "SELECT ESTADO from `ENCOMENDA` where `ID_ENC` = $enc_id");
+
+		if (!$sql_get_state)
+	    {
+	      die('Erro a ir buscar o estado da pizza: ' . mysqli_error($con));
+	    }
+
+	    echo "encontrou o estado" .  "<br>";
+
+		$row = mysqli_fetch_array($sql_get_state, MYSQLI_NUM);
+		$current_state = $row[0];
+
+		$next_state = $current_state + 1;
+
+		$sql_change_state = mysqli_query($con, "UPDATE `ENCOMENDA` SET `ESTADO`= $next_state WHERE `ID_ENC` = $enc_id");
+
+		if (!$sql_change_state)
+	    {
+	      die('Estado da encomenda não foi actualizado: ' . mysqli_error($con));
+	    } 
+
+	    echo "mudou o estado!" .  "<br>";
+	}
 ?>
 
 <html>
@@ -38,40 +70,41 @@
 			
 			<div class="row">
 				<div class="col s10 m10 l10 offset-s1 offset-m1 offset-l1">
-					
-					<table>
-						<thead>
-							<tr>
-								<th data-field="id">Id Enc</th>
-								<th data-field="id">Id Cliente</th>
-								<th data-field="id">Data</th>
-								<th data-field="id">Preço</th>
-								<th data-field="id">Estado</th>
-								<th data-field="id">Btn</th>
-							</tr>
-						</thead>
-						<tbody>
-						<?php
-							include "php/connect.php";
-							
-							$sql_get_encs = " SELECT * FROM ENCOMENDA";
+					<form method="POST">
+						<table>
+							<thead>
+								<tr>
+									<th data-field="id">Id Enc</th>
+									<th data-field="id">Id Cliente</th>
+									<th data-field="id">Data</th>
+									<th data-field="id">Preço</th>
+									<th data-field="id">Estado</th>
+									<th data-field="id">Btn</th>
+								</tr>
+							</thead>
+							<tbody>
+							<?php
+								include "php/connect.php";
+								
+								$sql_get_encs = " SELECT * FROM ENCOMENDA";
 
-							$result = mysqli_query($con, $sql_get_encs);
+								$result = mysqli_query($con, $sql_get_encs);
 
-							if (!$result){
-								die('Error: ' . mysqli_error($con));
-							}
+								if (!$result){
+									die('Error: ' . mysqli_error($con));
+								}
 
-							$row = mysqli_fetch_assoc($result);
-
-							while ( $row != null){ 
-								echo "<tr><td>" . $row['ID_ENC'] . "</td><td> ". $row['CLI_ID_CLIENTE'] . "</td><td>" . $row['DATA'] . "</td><td>" . $row['PRECO'] . "</td><td>" . $row['ESTADO'] . "</td><td>" . "<a class='waves-effect waves-light btn' type='submit' name='" . $row['ID_ENC'] . "'>Btn!</a>" . "</td></tr>";
 								$row = mysqli_fetch_assoc($result);
-							}
-						?>
 
-						</tbody>
-					</table>
+								while ( $row != null){ 
+									echo "<tr><td>" . $row['ID_ENC'] . "</td><td> ". $row['CLI_ID_CLIENTE'] . "</td><td>" . $row['DATA'] . "</td><td>" . $row['PRECO'] . "</td><td>" . $row['ESTADO'] . "</td><td>" . "<button class='waves-effect waves-light btn' type='submit' name='state_changer' value='" . $row['ID_ENC'] . "'>Btn!</button>" . "</td></tr>";
+									$row = mysqli_fetch_assoc($result);
+								}
+							?>
+
+							</tbody>
+						</table>
+					</form>
 
 				</div>
 			</div>
