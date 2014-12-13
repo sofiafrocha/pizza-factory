f50b704a635697ebf709dd6b27e82c3d8bc1e4b1
Mariana <marianne.lage@gmail.com>
2014-12-13 16:27:38 +0000

    we haz preçoooos

--- php/encomenda.php
+++ php/encomenda.php
@@ -2,17 +2,17 @@
 	session_start();
 	include "connect.php";
 
-	echo "Sofia is on the house." . "<br>";
+	echo "Mariana is taking control" . "<br>";
 	echo $_SESSION['username'] . " está a fazer uma encomenda" ."<br>";
 
-
+/*
 	$user_id = $_SESSION['user_id'];
     $sql_enc = mysqli_query($con, "INSERT INTO ENCOMENDA (CLI_ID_CLIENTE, ESTADO) VALUES ($user_id, 0)");
 
 	if (!$sql_enc)
     {
       die('Encomenda não inserida: ' . mysqli_error($con));
-    } 
+    } */
 
 	echo "encomenda inserida" . "<br>";
 
@@ -31,14 +31,14 @@
 
 	echo $_SESSION['username']." fez a encomenda número " . $id_enc . "<br>"; 
 
-    //fazer uma pizza
+  /*  //fazer uma pizza
 	$sql_pizza="INSERT INTO PIZZA (id_enc, estado, tamanho, massa, tomate, queijo) VALUES ('$id_enc', 0, 'individual', 'alta', 1, 1)";
 
 	if (!mysqli_query($con,$sql_pizza))
     {
       die('Error: ' . mysqli_error($con));
     } 
-
+*/
 	echo "inseriu pizza" . "<br>";
 
     //upa upa variáveis
@@ -46,13 +46,80 @@
 	    echo "passou MASSA: " . $_POST['massa']. "<br>";
 	    echo "passou TAMANHO: " . $_POST['tamanho']." oi"."<br>";
 	    echo "passou TOMATE: " . $_POST['tomate'] . "<br>";
-	    echo "passou QUEIJO: " . $_POST['queijo']." smile bitch"."<br>";
-		echo "passou INGREDIENTE: " . $_POST['ingrediente']." tchau" . "<br>";
+	    echo "passou QUEIJO: " . $_POST['queijo']."<br>";
+		echo "passou INGREDIENTE: " . $_POST['ingrediente']. "<br>";
 	} else {
 	    echo "no";
 	}
 
 
+    //preço
+    $preco = 0;
+
+    //preco massa
+
+    if ($_POST['massa'] == "alta" || $_POST['massa'] == "fina")
+    {
+        $preco = $preco + 1;
+        echo $preco."€"."<br>";
+    } 
+    elseif ($_POST['massa'] == "rolling") {
+       $preco = $preco + 1.5;
+       echo $preco."€"."<br>";
+    } 
+    else {
+        echo "não selecionaste massa, pois não?"."<br>";
+    } 
+
+
+    //preco queijo e molhos
+    if ($_POST['tomate'] == "yes")
+    {
+        $preco = $preco + 1;
+        echo $preco."€"."<br>";
+    } 
+    elseif ($_POST['tomate'] == "no") {
+       $preco = $preco;
+       echo $preco."€"."<br>";
+    } 
+    else {
+        echo "não selecionaste tomate, pois não?"."<br>";
+    } 
+
+    if ($_POST['queijo'] == "mozarella")
+    {
+        $preco = $preco + 1;
+        echo $preco."€"."<br>";
+    } 
+    elseif ($_POST['queijo'] == "cabra") {
+       $preco = $preco + 1.5;
+       echo $preco."€"."<br>";
+    } 
+    elseif ($_POST['queijo'] == "nenhum") {
+       $preco = $preco;
+       echo $preco."€"."<br>";
+    } 
+    else {
+        echo "não selecionaste quejo, pois não?"."<br>";
+    } 
+
+    //preco tamanho
+    if ($_POST['tamanho'] == "individual")
+    {
+        $preco = $preco + 4;
+        echo $preco."€"."<br>";
+    } 
+    elseif ($_POST['tamanho'] == "media") {
+       $preco = $preco + 6;
+       echo $preco."€"."<br>";
+    } 
+    elseif ($_POST['tamanho'] == "grande") {
+       $preco = $preco + 8;
+       echo $preco."€"."<br>";
+    } 
+    else {
+        echo "não selecionaste o tamanho, pois não?"."<br>";
+    } 
 
     //encontrar id da pizza que acabámos de fazer -_-
 	//listar todos, ordenar por data e limitar ao primeiro
@@ -68,7 +135,7 @@
 	$id_pizza= $row[0];
 	
 	echo "encontrou o id da pizza: " . $id_pizza . "<br>";
-
+/*
 	//por ingredientes na pizza
 	$sql_ingredientes="INSERT INTO PIZZA_has_INGREDIENTE (id_ingrediente, id_pizza) VALUES ('1', '$id_pizza'), ('2', '$id_pizza'), ('3','$id_pizza'), ('4', '$id_pizza')";
     $result=mysqli_query($con,$sql_ingredientes);
@@ -78,7 +145,7 @@
       die('Erro a por os ingredientes: ' . mysqli_error($con));
     }
 
-	echo "pôs os ingredientes " . "<br>"; 
+	echo "pôs os ingredientes " . "<br>"; */
 	
 	mysqli_close($db);
 
